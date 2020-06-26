7c4f1c000801f7ddbbbdb7d401db3eab773ba73d
BaurzhanKadis <37457582+BaurzhanKadis@users.noreply.github.com>
2020-06-26 16:53:57 +0300

    add muted

--- client/src/components/Loaded/Loaded.js
+++ client/src/components/Loaded/Loaded.js
@@ -1,22 +1,11 @@
-import React, {useEffect} from 'react';
-// import ReactPlayer from 'react-player';
+import React from 'react';
+import ReactPlayer from 'react-player';
 import './Loaded.css';
 
 const Loaded = () => {
-  // useEffect(() => {
-  //   async function fetchMyAPI() {
-  //     let response = await fetch('video/load.mp4')
-  //   }
-  //   fetchMyAPI()
-  // }, [])
-  const lll = <video src="video/load.mp4" autoPlay width="100%" height="100%"></video>
-  // useEffect( async () => {
-  //   await lll;
-  //  });
   return (
     <div className="Loaded">
-      {/* <ReactPlayer url="video/load.mp4" playing={true} loop width='100%' height='100%'/> */}
-      {lll}
+      <ReactPlayer url="video/load.mp4" playing={true} muted width='100%' height='100%'/>
     </div>
   );
 };
